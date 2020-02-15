301555a62b44ba5ee9bb4d65f484f3abba0791c3
Javier <jmarsican@gmail.com>
2020-02-15 19:32:19 -0300

    dinamically add cards

--- app/src/main/java/com/javiermarsicano/memogame/views/gameplay/GameplayFragment.kt
+++ app/src/main/java/com/javiermarsicano/memogame/views/gameplay/GameplayFragment.kt
@@ -5,11 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import com.javiermarsicano.memogame.R
-import kotlinx.android.synthetic.main.fragment_gameplay.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -22,28 +19,30 @@ class GameplayFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            height = 3
-            width = 3
+            height = 4
+            width = 4
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_gameplay, container, false) as ConstraintLayout
+        val view = inflater.inflate(R.layout.fragment_gameplay, container, false) as ViewGroup
 
 
         ids = ArrayList(width*height)
 
         val matrix = Array(width) {IntArray(height)}
         for (i in 0 until height) {
+            val row = inflater.inflate(R.layout.row, view, false) as ViewGroup
             for (j in 0 until width) {
                 val card = inflater.inflate(R.layout.memocard, null)
                 val id = View.generateViewId()
                 card.id = id
                 matrix[j][i] = id
                 ids?.add(id)
-                view.addView(card)
+                row.addView(card)
             }
+            view.addView(row)
         }
 
 
@@ -53,7 +52,6 @@ class GameplayFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        flow_helper.referencedIds = ids?.toIntArray()
 
     }
 
