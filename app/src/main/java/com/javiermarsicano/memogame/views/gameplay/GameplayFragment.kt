bb814f1294867a3d4aea510dc921ea33de098716
Javier <jmarsican@gmail.com>
2020-02-15 18:10:18 -0300

    add cards programmatically but cannot set distribution

--- app/src/main/java/com/javiermarsicano/memogame/views/gameplay/GameplayFragment.kt
+++ app/src/main/java/com/javiermarsicano/memogame/views/gameplay/GameplayFragment.kt
@@ -1,44 +1,50 @@
 package com.javiermarsicano.memogame.views.gameplay
 
 
-import android.animation.AnimatorInflater
-import android.animation.AnimatorSet
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import com.javiermarsicano.memogame.R
-import kotlinx.android.synthetic.main.card_front.*
 import kotlinx.android.synthetic.main.fragment_gameplay.*
-import kotlinx.android.synthetic.main.fragment_gameplay.card_cover
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class GameplayFragment : Fragment() {
-    private var height: String? = null
-    private var width: String? = null
-
-    private var mSetRightOut: AnimatorSet? = null
-    private var mSetLeftIn: AnimatorSet? = null
-    private var mIsBackVisible = false
+    private var height: Int = 0
+    private var width: Int = 0
+    private var ids: ArrayList<Int>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            height = it.getString(ARG_PARAM1)
-            width = it.getString(ARG_PARAM2)
+            height = 3
+            width = 3
         }
-
-//        mSetRightOut = AnimatorInflater.loadAnimator(context, R.animator.out_animation) as AnimatorSet
-//        mSetLeftIn = AnimatorInflater.loadAnimator(context, R.animator.in_animation) as AnimatorSet
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_gameplay, container, false)
+        val view = inflater.inflate(R.layout.fragment_gameplay, container, false) as ConstraintLayout
 
+
+        ids = ArrayList(width*height)
+
+        val matrix = Array(width) {IntArray(height)}
+        for (i in 0 until height) {
+            for (j in 0 until width) {
+                val card = inflater.inflate(R.layout.memocard, null)
+                val id = View.generateViewId()
+                card.id = id
+                matrix[j][i] = id
+                ids?.add(id)
+                view.addView(card)
+            }
+        }
 
 
         return view
@@ -47,26 +53,11 @@ class GameplayFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        card_view.setOnClickListener {
+        flow_helper.referencedIds = ids?.toIntArray()
 
-        }
     }
 
-    fun flipCard(view: View) {
-        if (!mIsBackVisible) {
-            mSetRightOut?.setTarget(card_cover)
-            mSetLeftIn?.setTarget(card_front)
-            mSetRightOut?.start()
-            mSetLeftIn?.start()
-            mIsBackVisible = true
-        } else {
-            mSetRightOut?.setTarget(card_front)
-            mSetLeftIn?.setTarget(card_cover)
-            mSetRightOut?.start()
-            mSetLeftIn?.start()
-            mIsBackVisible = false
-        }
-    }
+
 
     companion object {
         /**
